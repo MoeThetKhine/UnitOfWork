c32763a7dc6f9596899c35f0e4dcd5e5f8a21e01
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-23 00:11:29 +0630

    -

--- DotNet8.UnitOfWork.Utils/Result.cs
+++ DotNet8.UnitOfWork.Utils/Result.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.UnitOfWork.Utils
+﻿namespace DotNet8.UnitOfWork.Utils
 {
 	internal class Result
 	{

9decdc31173529035b5de83dc01cf4ad2582f80a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 23:44:32 +0630

    Feat : Add BlogListModelV1

--- DotNet8.UnitOfWork.DTOs/Feature/Blog/BlogListModel.cs
+++ DotNet8.UnitOfWork.DTOs/Feature/Blog/BlogListModel.cs
@@ -11,3 +11,9 @@ public class BlogListModel
 }
 
 #endregion
+
+public class BlogListModelV1
+{
+	public IQueryable<BlogModel> DataLst { get; set; }
+	public PageSettingModel PageSetting { get; set; }
+}

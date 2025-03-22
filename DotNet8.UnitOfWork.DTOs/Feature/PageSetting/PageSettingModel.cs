130fecbeed6487f76ef2fc8259a7b7c5f2efa469
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 23:45:58 +0630

    Feat : Add PageSettingModel

--- DotNet8.UnitOfWork.DTOs/Feature/PageSetting/PageSettingModel.cs
+++ DotNet8.UnitOfWork.DTOs/Feature/PageSetting/PageSettingModel.cs
@@ -2,4 +2,19 @@
 
 public class PageSettingModel
 {
+	public int PageNo { get; set; }
+
+	public int PageSize { get; set; }
+
+	public int PageCount { get; set; }
+
+	public int TotalCount { get; set; }
+
+	public PageSettingModel(int pageNo, int pageSize, int pageCount, int totalCount)
+	{
+		PageNo = pageNo;
+		PageSize = pageSize;
+		PageCount = pageCount;
+		TotalCount = totalCount;
+	}
 }

017d45af0aa98f65093c18537269143fd86fc062
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 22:32:33 +0630

    -

--- DotNet8.UnitOfWork.DbService/AppDbContextModels/Job.cs
+++ DotNet8.UnitOfWork.DbService/AppDbContextModels/Job.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace DotNet8.UnitOfWork.DbService.AppDbContextModels;
+﻿namespace DotNet8.UnitOfWork.DbService.AppDbContextModels;
 
 public partial class Job
 {

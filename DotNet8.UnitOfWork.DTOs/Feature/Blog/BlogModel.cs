1d1acea244bba6e1d109ae9761eff32d6182387e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 23:36:23 +0630

    -

---
+++ DotNet8.UnitOfWork.DTOs/Feature/Blog/BlogModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.UnitOfWork.DTOs.Feature.Blog
+{
+	internal class BlogModel
+	{
+	}
+}

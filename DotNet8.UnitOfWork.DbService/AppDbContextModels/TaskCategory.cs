d4bd8647ccd1b7332a7e470cc03263617589f108
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 22:15:54 +0630

    -

---
+++ DotNet8.UnitOfWork.DbService/AppDbContextModels/TaskCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet8.UnitOfWork.DbService.AppDbContextModels;
+
+public partial class TaskCategory
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    public bool? IsDelete { get; set; }
+
+    public virtual ICollection<ToDoList> ToDoLists { get; set; } = new List<ToDoList>();
+}

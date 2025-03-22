22a2384c3eec212bc98685d871479a5d9d4c4316
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 23:52:40 +0630

    -

--- DotNet8.UnitOfWork.Utils/Enums/EnumStatusCode.cs
+++ DotNet8.UnitOfWork.Utils/Enums/EnumStatusCode.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.UnitOfWork.Utils.Enums
+﻿namespace DotNet8.UnitOfWork.Utils.Enums
 {
 	internal class EnumStatusCode
 	{

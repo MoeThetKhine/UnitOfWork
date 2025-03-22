e72f3c69f69e2a1f6c44b8017bc1cf40c063fde8
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 23:37:55 +0630

    -

--- DotNet8.UnitOfWork.DTOs/Feature/PageSetting/PageSettingModel.cs
+++ DotNet8.UnitOfWork.DTOs/Feature/PageSetting/PageSettingModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.UnitOfWork.DTOs.Feature.PageSetting
+﻿namespace DotNet8.UnitOfWork.DTOs.Feature.PageSetting
 {
 	internal class PageSettingModel
 	{

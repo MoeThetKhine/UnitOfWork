8c4c14fa9e03ccea784e027a2bba30d9dd9f6c76
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 22:35:22 +0630

    -

--- DotNet8.UnitOfWork.DbService/AppDbContextModels/AppDbContext.cs
+++ DotNet8.UnitOfWork.DbService/AppDbContextModels/AppDbContext.cs
@@ -13,7 +13,9 @@ public partial class AppDbContext : DbContext
     {
     }
 
-    public virtual DbSet<AggregatedCounter> AggregatedCounters { get; set; }
+	#region DbSet
+
+	public virtual DbSet<AggregatedCounter> AggregatedCounters { get; set; }
 
     public virtual DbSet<Counter> Counters { get; set; }
 
@@ -41,7 +43,9 @@ public partial class AppDbContext : DbContext
 
     public virtual DbSet<ToDoList> ToDoLists { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	#endregion
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=.;Database=DotNetTrainingBatch5;User Id=sa;Password=sasa@123;TrustServerCertificate=True;");
 

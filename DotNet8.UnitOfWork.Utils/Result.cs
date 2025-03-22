0781db588086fac02f8f5eb3a6a4cf5df438282b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-23 00:16:57 +0630

    Feat : Add Success

--- DotNet8.UnitOfWork.Utils/Result.cs
+++ DotNet8.UnitOfWork.Utils/Result.cs
@@ -8,4 +8,14 @@ public class Result<T>
 	public string Message {  get; set; }
 	public bool IsSuccess {  get; set; }
 	public EnumStatusCode StatusCode { get; set; }
+
+	public static Result<T> Success(string message = "Success", EnumStatusCode statusCode = EnumStatusCode.Success)
+	{
+		return new Result<T>
+		{
+			IsSuccess = true,
+			Message = message,
+			StatusCode = statusCode
+		};
+	}
 }

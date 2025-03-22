cb916072db27bab3d17ea735cea791adcb31a83b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 23:38:45 +0630

    -

--- DotNet8.UnitOfWork.DTOs/Feature/Blog/BlogRequestModel.cs
+++ DotNet8.UnitOfWork.DTOs/Feature/Blog/BlogRequestModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.UnitOfWork.DTOs.Feature.Blog
+﻿namespace DotNet8.UnitOfWork.DTOs.Feature.Blog
 {
 	internal class BlogRequestModel
 	{

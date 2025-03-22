8a6b72d91a770eb38562cc152a565f0209b61c14
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 23:41:52 +0630

    Feat : Add Blog Model

--- DotNet8.UnitOfWork.DTOs/Feature/Blog/BlogModel.cs
+++ DotNet8.UnitOfWork.DTOs/Feature/Blog/BlogModel.cs
@@ -2,4 +2,13 @@
 
 public class BlogModel
 {
+	public long BlogId { get; set; }
+
+	public string BlogTitle { get; set; } = null!;
+
+	public string BlogAuthor { get; set; } = null!;
+
+	public string BlogContent { get; set; } = null!;
+
+	public bool? DeleteFlag { get; set; }
 }

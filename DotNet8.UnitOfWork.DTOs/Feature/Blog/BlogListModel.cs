c32bfad319d035bfb999ebe22f7e58946252b15e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 23:43:53 +0630

    Feat : Add BlogListModel

--- DotNet8.UnitOfWork.DTOs/Feature/Blog/BlogListModel.cs
+++ DotNet8.UnitOfWork.DTOs/Feature/Blog/BlogListModel.cs
@@ -1,5 +1,9 @@
-﻿namespace DotNet8.UnitOfWork.DTOs.Feature.Blog;
+﻿using DotNet8.UnitOfWork.DTOs.Feature.PageSetting;
+
+namespace DotNet8.UnitOfWork.DTOs.Feature.Blog;
 
 public class BlogListModel
 {
+	public IEnumerable<BlogModel> DataLst { get; set; }
+	public PageSettingModel PageSetting { get; set; }
 }

6429842d1634c6d7a983b00e1b8b0a75dcae0c9c
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 22:16:09 +0630

    -

---
+++ DotNet8.UnitOfWork.DbService/AppDbContextModels/ToDoList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet8.UnitOfWork.DbService.AppDbContextModels;
+
+public partial class ToDoList
+{
+    public int TaskId { get; set; }
+
+    public string TaskTitle { get; set; } = null!;
+
+    public string? TaskDescription { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public byte? PriorityLevel { get; set; }
+
+    public string? Status { get; set; }
+
+    public DateOnly? DueDate { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+
+    public DateTime? CompletedDate { get; set; }
+
+    public virtual TaskCategory? Category { get; set; }
+}

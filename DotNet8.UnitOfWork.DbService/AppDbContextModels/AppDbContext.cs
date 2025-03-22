adc794ef72487948ee7c4336ec4da41bcee79243
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 23:29:38 +0630

    -

--- DotNet8.UnitOfWork.DbService/AppDbContextModels/AppDbContext.cs
+++ DotNet8.UnitOfWork.DbService/AppDbContextModels/AppDbContext.cs
@@ -242,6 +242,8 @@ public partial class AppDbContext : DbContext
 
 		#endregion
 
+		#region TaskCategory
+
 		modelBuilder.Entity<TaskCategory>(entity =>
         {
             entity.HasKey(e => e.CategoryId).HasName("PK__TaskCate__19093A2B739DC4B2");
@@ -254,7 +256,9 @@ public partial class AppDbContext : DbContext
                 .IsUnicode(false);
         });
 
-        modelBuilder.Entity<TblBlog>(entity =>
+		#endregion
+
+		modelBuilder.Entity<TblBlog>(entity =>
         {
             entity.HasKey(e => e.BlogId);
 

c8d99e78fda0c72177261e83071c105499ebd682
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 23:42:59 +0630

    -

--- DotNet8.UnitOfWork.DTOs/Feature/Blog/BlogRequestModel.cs
+++ DotNet8.UnitOfWork.DTOs/Feature/Blog/BlogRequestModel.cs
@@ -1,8 +1,12 @@
 ﻿namespace DotNet8.UnitOfWork.DTOs.Feature.Blog;
 
+#region BlogRequestModel
+
 public class BlogRequestModel
 {
 	public string BlogTitle { get; set; } = null!;
 	public string BlogAuthor { get; set; } = null!;
 	public string BlogContent { get; set; } = null!;
 }
+
+#endregion

1daab51714431c494941fe7900f5eeb2b6321b44
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-23 01:04:18 +0630

    -

--- DotNet8.UnitOfWork.Utils/GlobalUsing.cs
+++ DotNet8.UnitOfWork.Utils/GlobalUsing.cs
@@ -1,12 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.UnitOfWork.Utils
-{
-	internal class GlobalUsing
-	{
-	}
-}
+﻿

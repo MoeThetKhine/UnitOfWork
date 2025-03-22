d172abbea4eace02184a5eff6d99b8486a97bae7
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 22:16:02 +0630

    -

---
+++ DotNet8.UnitOfWork.DbService/AppDbContextModels/TblBlog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet8.UnitOfWork.DbService.AppDbContextModels;
+
+public partial class TblBlog
+{
+    public long BlogId { get; set; }
+
+    public string BlogTitle { get; set; } = null!;
+
+    public string BlogAuthor { get; set; } = null!;
+
+    public string BlogContent { get; set; } = null!;
+
+    public bool? DeleteFlag { get; set; }
+}

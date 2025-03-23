3599691cba4c009d54901b3944744a860d9134ea
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-23 11:57:59 +0630

    Feat : Add ToObject

--- DotNet8.UnitOfWork.Shared/DevCode.cs
+++ DotNet8.UnitOfWork.Shared/DevCode.cs
@@ -6,4 +6,8 @@ public static class DevCode
 {
 	public static string ToJson(this object obj) =>
 		JsonConvert.SerializeObject(obj, Formatting.Indented);
+
+	public static T ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr)!;
+
+
 }

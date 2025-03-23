a9af604d24710c96a5da1a1144b32d8fe2d9b0ae
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-23 12:03:59 +0630

    -

--- DotNet8.UnitOfWork.Shared/DevCode.cs
+++ DotNet8.UnitOfWork.Shared/DevCode.cs
@@ -2,6 +2,8 @@
 
 namespace DotNet8.UnitOfWork.Shared;
 
+#region DevCode
+
 public static class DevCode
 {
 	public static string ToJson(this object obj) =>
@@ -16,5 +18,6 @@ public static class DevCode
 	{
 		return source.Skip((pageNo - 1) * pageSize).Take(pageSize);
 	}
-
 }
+
+#endregion

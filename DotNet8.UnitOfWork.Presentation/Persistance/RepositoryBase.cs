75914c519b84ae4d995db9a425892706a0a2f2e0
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-24 12:34:13 +0630

    Format Code

--- DotNet8.UnitOfWork.Presentation/Persistance/RepositoryBase.cs
+++ DotNet8.UnitOfWork.Presentation/Persistance/RepositoryBase.cs
@@ -1,14 +1,13 @@
-﻿namespace DotNet8.UnitOfWork.Presentation.Persistance
-{
-	public class RepositoryBase<T> : IRepositoryBase<T> where T : class
-	{
-		internal readonly AppDbContext _context;
-		internal DbSet<T> _dbSet;
+﻿namespace DotNet8.UnitOfWork.Presentation.Persistance;
 
-		public RepositoryBase(AppDbContext context)
-		{
-			_context = context;
-			_dbSet = context.Set<T>();
-		}
+public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+{
+	internal readonly AppDbContext _context;
+	internal DbSet<T> _dbSet;
+
+	public RepositoryBase(AppDbContext context)
+	{
+		_context = context;
+		_dbSet = context.Set<T>();
 	}
 }

f8a4965bd240ca2bedaa07182bb19e9a0af1459b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-24 13:08:25 +0630

    Feat : Add UnitOfWork

--- DotNet8.UnitOfWork.Presentation/Persistance/UnitOfWork.cs
+++ DotNet8.UnitOfWork.Presentation/Persistance/UnitOfWork.cs
@@ -1,5 +1,17 @@
 ﻿namespace DotNet8.UnitOfWork.Presentation.Persistance;
 
-public class UnitOfWork
+public class UnitOfWork : IUnitOfWork
 {
+	public readonly AppDbContext _context;
+	public IBlogRepository BlogRepository { get; set; }
+
+
+	public UnitOfWork(AppDbContext context)
+	{
+		_context = context;
+		BlogRepository ??= new BlogRepository(context);
+	}
+
+
 }
+

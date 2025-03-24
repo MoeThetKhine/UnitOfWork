d6aa2b5973e983a8adafea9e7b98d8f4e76a8ef2
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-24 12:59:44 +0630

    Format Code

--- DotNet8.UnitOfWork.Presentation/Persistance/Repositories/BlogRepository.cs
+++ DotNet8.UnitOfWork.Presentation/Persistance/Repositories/BlogRepository.cs
@@ -1,9 +1,7 @@
-﻿namespace DotNet8.UnitOfWork.Presentation.Persistance.Repositories
-{
-	public class BlogRepository : RepositoryBase<TblBlog>, IBlogRepository
-	{
-		public BlogRepository(AppDbContext context)
-			: base(context) { }
-	}
+﻿namespace DotNet8.UnitOfWork.Presentation.Persistance.Repositories;
 
+public class BlogRepository : RepositoryBase<TblBlog>, IBlogRepository
+{
+	public BlogRepository(AppDbContext context)
+		: base(context) { }
 }

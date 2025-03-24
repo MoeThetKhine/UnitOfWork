7db260e4a00e8af262ee212d865538ba4cd0b8b9
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-24 12:24:31 +0630

    Feat : Add RepositoryBase

--- DotNet8.UnitOfWork.Presentation/Persistance/RepositoryBase.cs
+++ DotNet8.UnitOfWork.Presentation/Persistance/RepositoryBase.cs
@@ -1,6 +1,17 @@
-﻿namespace DotNet8.UnitOfWork.Presentation.Persistance
+﻿using DotNet8.UnitOfWork.DbService.AppDbContextModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8.UnitOfWork.Presentation.Persistance
 {
-	public class RepositoryBase
+	public class RepositoryBase<T> : IRepositoryBase<T> where T : class
 	{
+		internal readonly AppDbContext _context;
+		internal DbSet<T> _dbSet;
+
+		public RepositoryBase(AppDbContext context)
+		{
+			_context = context;
+			_dbSet = context.Set<T>();
+		}
 	}
 }

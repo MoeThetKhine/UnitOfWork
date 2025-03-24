55f67f50401ba20f68ddd62e9ad9d2241af69235
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-24 12:23:52 +0630

    Feat : Add BlogRepository

--- DotNet8.UnitOfWork.Presentation/Persistance/Repositories/BlogRepository.cs
+++ DotNet8.UnitOfWork.Presentation/Persistance/Repositories/BlogRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace DotNet8.UnitOfWork.Presentation.Persistance.Repositories
+﻿using DotNet8.UnitOfWork.DbService.AppDbContextModels;
+
+namespace DotNet8.UnitOfWork.Presentation.Persistance.Repositories
 {
-	public class BlogRepository
+	public class BlogRepository : RepositoryBase<TblBlog>, IBlogRepository
 	{
+		public BlogRepository(AppDbContext context)
+			: base(context) { }
 	}
+
 }

f1175ce7eae18a1cc8351bcd39a50ba4cd774fa0
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-24 12:23:25 +0630

    Feat : Add UnitOfWork

--- DotNet8.UnitOfWork.Presentation/UnitOfWork.cs
+++ DotNet8.UnitOfWork.Presentation/UnitOfWork.cs
@@ -1,6 +1,24 @@
-﻿namespace DotNet8.UnitOfWork.Presentation
+﻿using DotNet8.UnitOfWork.DbService.AppDbContextModels;
+using DotNet8.UnitOfWork.Presentation.Persistance.Repositories;
+using DotNet8.UnitOfWork.Presentation.Persistance;
+
+namespace DotNet8.UnitOfWork.Presentation
 {
-	public class UnitOfWork
+	public class UnitOfWork : IUnitOfWork
 	{
+		public readonly AppDbContext _context;
+		public IBlogRepository BlogRepository { get; set; }
+
+		#region UnitOfWork
+
+		public UnitOfWork(AppDbContext context)
+		{
+			_context = context;
+			BlogRepository ??= new BlogRepository(context);
+		}
+
+		#endregion
+
 	}
+
 }

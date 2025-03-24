f4101c3820fddec68f27fadb9b2bb9fca32d5c68
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-24 12:58:38 +0630

    -

--- DotNet8.UnitOfWork.Presentation/Persistance/Repositories/BlogRepository.cs
+++ DotNet8.UnitOfWork.Presentation/Persistance/Repositories/BlogRepository.cs
@@ -1,6 +1,4 @@
-﻿using DotNet8.UnitOfWork.DbService.AppDbContextModels;
-
-namespace DotNet8.UnitOfWork.Presentation.Persistance.Repositories
+﻿namespace DotNet8.UnitOfWork.Presentation.Persistance.Repositories
 {
 	public class BlogRepository : RepositoryBase<TblBlog>, IBlogRepository
 	{

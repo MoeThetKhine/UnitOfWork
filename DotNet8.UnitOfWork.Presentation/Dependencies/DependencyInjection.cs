d2a450913abdcd44d7d925fa2bf3ead9e4d6652f
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-24 12:26:25 +0630

    Format Code

--- DotNet8.UnitOfWork.Presentation/Dependencies/DependencyInjection.cs
+++ DotNet8.UnitOfWork.Presentation/Dependencies/DependencyInjection.cs
@@ -2,25 +2,24 @@
 using DotNet8.UnitOfWork.Presentation.Persistance;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotNet8.UnitOfWork.Presentation.Dependencies
+namespace DotNet8.UnitOfWork.Presentation.Dependencies;
+
+public static class DependencyInjection
 {
-	public static class DependencyInjection
+	public static IServiceCollection AddDependencies(this IServiceCollection services)
 	{
-		public static IServiceCollection AddDependencies(this IServiceCollection services)
+		services.AddDbContext<AppDbContext>(opt =>
 		{
-			services.AddDbContext<AppDbContext>(opt =>
-			{
-				opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-			});
+			opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+		});
 
-			services.AddControllers().AddJsonOptions(opt =>
-			{
-				opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-			});
+		services.AddControllers().AddJsonOptions(opt =>
+		{
+			opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+		});
 
-			services.AddScoped<IUnitOfWork, UnitOfWork>();
+		services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-			return services;
-		}
+		return services;
 	}
 }

6a52892cb41aaa0289355a62d380d5b16b631742
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-24 12:24:53 +0630

    Feat : Add DependencyInjection

--- DotNet8.UnitOfWork.Presentation/Dependencies/DependencyInjection.cs
+++ DotNet8.UnitOfWork.Presentation/Dependencies/DependencyInjection.cs
@@ -1,6 +1,26 @@
-﻿namespace DotNet8.UnitOfWork.Presentation.Dependencies
+﻿using DotNet8.UnitOfWork.DbService.AppDbContextModels;
+using DotNet8.UnitOfWork.Presentation.Persistance;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8.UnitOfWork.Presentation.Dependencies
 {
-	public class DependencyInjection
+	public static class DependencyInjection
 	{
+		public static IServiceCollection AddDependencies(this IServiceCollection services)
+		{
+			services.AddDbContext<AppDbContext>(opt =>
+			{
+				opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+			});
+
+			services.AddControllers().AddJsonOptions(opt =>
+			{
+				opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+			});
+
+			services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+			return services;
+		}
 	}
 }

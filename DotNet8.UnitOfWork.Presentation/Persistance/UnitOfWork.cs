f3361b283073df97eae35df27f145e8bea5923e8
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-24 13:08:36 +0630

    -

--- DotNet8.UnitOfWork.Presentation/Persistance/UnitOfWork.cs
+++ DotNet8.UnitOfWork.Presentation/Persistance/UnitOfWork.cs
@@ -5,6 +5,7 @@ public class UnitOfWork : IUnitOfWork
 	public readonly AppDbContext _context;
 	public IBlogRepository BlogRepository { get; set; }
 
+	#region UnitOfWork
 
 	public UnitOfWork(AppDbContext context)
 	{
@@ -12,6 +13,7 @@ public class UnitOfWork : IUnitOfWork
 		BlogRepository ??= new BlogRepository(context);
 	}
 
+	#endregion
 
 }
 

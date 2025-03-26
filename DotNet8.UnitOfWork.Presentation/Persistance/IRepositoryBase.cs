df9d914d1da8c7483fbf6d1f4908ad1b8c667d88
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-27 00:23:03 +0630

    -

--- DotNet8.UnitOfWork.Presentation/Persistance/IRepositoryBase.cs
+++ DotNet8.UnitOfWork.Presentation/Persistance/IRepositoryBase.cs
@@ -2,4 +2,8 @@
 
 public interface IRepositoryBase<T> where T : class
 {
+	IQueryable<T> Query(Expression<Func<T, bool>>? expression = null);
+	Task AddAsync(T entity, CancellationToken cs = default);
+	void Add(T entity);
+
 }

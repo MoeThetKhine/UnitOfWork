e09a94944a22a68779222a0315ec86825ae2cb1f
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-27 10:51:04 +0630

    Feat : Add ChangeModel

--- DotNet8.UnitOfWork.Presentation/ChangeModel.cs
+++ DotNet8.UnitOfWork.Presentation/ChangeModel.cs
@@ -1,6 +1,17 @@
-﻿namespace DotNet8.UnitOfWork.Presentation
+﻿using DotNet8.UnitOfWork.DTOs.Feature.Blog;
+
+namespace DotNet8.UnitOfWork.Presentation
 {
-	public class ChangeModel
+	public static class ChangeModel
 	{
+		public static TblBlog Change(this BlogRequestModel requestModel)
+		{
+			return new TblBlog
+			{
+				BlogTitle = requestModel.BlogTitle,
+				BlogAuthor = requestModel.BlogAuthor,
+				BlogContent = requestModel.BlogContent,
+			};
+		}
 	}
 }

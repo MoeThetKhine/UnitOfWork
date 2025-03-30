ee163f4fd41f7ac78ec98e157fada83a98f86026
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-30 19:10:14 +0630

    Feat : Add GetBlogsAsync in Controller

--- DotNet8.UnitOfWork.Presentation/Controllers/BlogController.cs
+++ DotNet8.UnitOfWork.Presentation/Controllers/BlogController.cs
@@ -1,7 +1,23 @@
-﻿namespace DotNet8.UnitOfWork.Presentation.Controllers;
+﻿using DotNet8.UnitOfWork.Shared;
+
+namespace DotNet8.UnitOfWork.Presentation.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 public class BlogController : ControllerBase
 {
+	public readonly IUnitOfWork _unitOfWork;
+
+	public BlogController(IUnitOfWork unitOfWork)
+	{
+		_unitOfWork = unitOfWork;
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetBlogsAsync(int pageNo, int pageSize, CancellationToken cancellationToken)
+	{
+		var query = _unitOfWork.BlogRepository.Query().Paginate(pageNo, pageSize);
+		var lst = await query.ToListAsync(cancellationToken);
+		return Ok(lst);
+	}
 }
